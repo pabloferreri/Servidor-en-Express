8bceda76c54a038dae7ff74bc7489c0855be952d
pabloferreri <pab.ferreri@gmail.com>
2020-12-05 00:20:44 -0300

    get babbage, berners Lee, clarke

--- index.js
+++ index.js
@@ -9,3 +9,15 @@ app.listen(3030,() => {
 app.get('/', (req,res) =>{
     res.sendFile(path.join(__dirname,'./index.html'))
 })
+
+app.get('/babbage', (req,res) =>{
+    res.sendFile(path.join(__dirname,'./babbage/babbage.html'))
+})
+
+app.get('/berners-lee', (req,res) =>{
+    res.sendFile(path.join(__dirname,'./berners-lee/berners-lee.html'))
+})
+
+app.get('/clarke', (req,res) =>{
+    res.sendFile(path.join(__dirname,'./clarke/clarke.html'))
+})
