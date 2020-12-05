43a802a5b154ab6eae60c22fb713b2642ae58320
pabloferreri <pab.ferreri@gmail.com>
2020-12-05 00:11:16 -0300

    ya agregue todos los html en sus respectivas carpetas

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ const path = require('path');
 app.listen(3030,() => {
     console.log('Servidor en puerto 3030.');
 })
+
+app.get('/', (req,res) =>{
+    res.sendFile(path.join(__dirname,'./index.html'))
+})
